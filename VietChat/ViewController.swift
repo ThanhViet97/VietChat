ccf5bcd549f292ef0fd1c095de54d7fab6bcf355
Thanh Việt <thanhvietbghl@gmail.com>
2019-09-11 14:41:35 +0700

    Create project VietChat

---
+++ VietChat/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  VietChat
+//
+//  Created by VietPhan on 9/11/19.
+//  Copyright © 2019 Phan Thanh Việt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
