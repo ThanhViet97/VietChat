5d1908125189395d4a7a24a09f2cf5fc4228b010
Thanh Việt <thanhvietbghl@gmail.com>
2019-09-11 17:32:48 +0700

    Handle frame set project

---
+++ VietChat/Scenes/Chat/view/ChatTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatTableViewCell.swift
+//  VietChat
+//
+//  Created by VietPhan on 9/11/19.
+//  Copyright © 2019 Phan Thanh Việt. All rights reserved.
+//
+
+import UIKit
+
+class ChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
